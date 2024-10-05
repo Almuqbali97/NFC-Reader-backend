229f7af1cc2cd48f968edaf64495dff3edff5822
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-10-05 19:58:09 +0400

    cors policies

--- index.js
+++ index.js
@@ -5,12 +5,62 @@ const { v4: uuidv4 } = require('uuid');  // Use uuid to generate unique IDs
 const app = express();
 const port = 5000;
 
-const corsOptions = {
-    origin: 'https://astonishing-dolphin-daa5ea.netlify.app',  // Allow only this origin
-    optionsSuccessStatus: 200
-};
 
-app.use(cors(corsOptions));
+const allowedOriginsDynamic = [
+    'https://mioc-client.onrender.com',
+    'https://mioc.netlify.app',
+    "https://main--mioc.netlify.app",
+    'https://mioc.org.om',
+    "https://astonishing-dolphin-daa5ea.netlify.app",
+];
+
+// else
+const allowedOriginsStatic = [
+    "https://astonishing-dolphin-daa5ea.netlify.app",
+    'http://localhost:5000',
+    'http://localhost:5173',
+    'https://mioc-website-client.vercel.app',
+    'https://mti.bankmuscat.com:6443/',
+    'https://smartpaytrns.bankmuscat.com/',
+    'https://spayuattrns.bmtest.om',
+];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    // Check if the origin is from Render or Netlify (dynamic origins)
+    if (allowedOriginsDynamic.includes(origin)) {
+        cors({
+            origin: function (origin, callback) {
+                if (!origin) return callback(null, true);
+                if (allowedOriginsDynamic.indexOf(origin) !== -1) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'));
+                }
+            },
+            methods: ['GET', 'PUT', 'POST', 'DELETE'],
+            allowedHeaders: ['Content-Type'],
+            credentials: true // Allow cookies to be sent
+        })(req, res, next);
+    } else {
+        // Apply the static CORS config for other origins
+        cors({
+            origin: allowedOriginsStatic,
+            methods: ['GET', 'PUT', 'POST', 'DELETE'],
+            allowedHeaders: ['Content-Type'],
+            credentials: true // Allow cookies to be sent
+        })(req, res, next);
+    }
+});
+
+
+// const corsOptions = {
+//     origin: 'https://astonishing-dolphin-daa5ea.netlify.app',  // Allow only this origin
+//     optionsSuccessStatus: 200
+// };
+
+// app.use(cors(corsOptions));
 app.use(express.json());
 
 // Mock Data
@@ -28,7 +78,7 @@ let companies = [
 
 // Get all employees
 app.get('/', (req, res) => {
-    res.send("hello From Api");
+    res.json("hello From Api");
 });
 app.get('/api/employees', (req, res) => {
     res.json(employees);
